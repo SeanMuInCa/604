90da2f15acf8da7c992812738c7ffb6e8c4b7c00
Mu <105630173+SeanMuInCa@users.noreply.github.com>
2024-03-15 12:35:46 -0600

    Add files via upload

---
+++ Delegates/Delegates/Collection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Delegates
+{
+    public class Collection
+    {
+        private int[] iArray = { 5, 10, 20 };
+        public enum Direction
+        {
+            FORWARD,
+            BACKWARD
+        }
+        //Create a delegate type
+        public delegate void DoSomethingToData(int x);
+        //A method to iterate through the data
+        //Use the delegate to pass a method into iterate
+        public void iterate(DoSomethingToData ds, Direction dir)
+        {
+            //for (int j = 0; j < iArray.Length; j++)
+            //{
+            //    //Console.WriteLine(iArray[j]);
+            //    //Call the instance of the delegate on the current data item.
+            //    ds(iArray[j]);
+            //}
+            switch (dir)
+            {
+                case Direction.FORWARD:
+                    for (int j = 0; j < iArray.Length; j++)
+                    {
+                        ds(iArray[j]);
+                    }
+                    break;
+                case Direction.BACKWARD:
+                    for (int j = iArray.Length - 1; j >= 0; j--)
+                    {
+                        ds(iArray[j]);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Delegates
+{
+    public class Program
+    {
+        public static void DisplayInt(int x)
+        {
+            Console.WriteLine(x);
+        }
+        public static void DisplaySquare(int x)
+        {
+            Console.WriteLine(x * x);
+        }
+        //Can we use a method where the signature does not match.
+        public static void DisplayAsDouble(double x)
+        {
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            Collection c = new Collection();
+            c.iterate(DisplayInt, Collection.Direction.BACKWARD);
+            //c.iterate(DisplayAsDouble); //Signature must match the delegate.
+        }
+    }
+}
